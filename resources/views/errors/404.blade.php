64642d07bbe604a4ad4bedf850748fb9f2e1190d
egor <egor@vershkoff.ru>
2017-01-04 01:51:56 +0300

    add error forms

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>404 Страница не найдена.</title>
+        <link rel="stylesheet" href="{{ url('/css/all.css')}}">
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="col-lg-10 col-lg-offset-2">
+                <div class="jumbotron">
+                    <h1>404 Страница не найдена</h1>
+                    <p>Страница, на которую вы пытаетесь попасть, вероятно не существует</p>
+                    <p><a class="btn btn-default btn-lg" href="/" role="button">на главную</a></p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
