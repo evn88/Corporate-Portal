5c3810dafdab90cdce8c919c54cbab6d22b65a0b
egor <egor@vershkoff.ru>
2017-01-04 15:07:01 +0300

    fix css

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>404 Страница не найдена.</title>
-        <link rel="stylesheet" href="{{ url('/css/all.css')}}">
+        <link rel="stylesheet" href="{{ url('/css/materialize.min.css')}}">
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -10,11 +10,11 @@
     </head>
     <body>
         <div class="container">
-            <div class="col-lg-10 col-lg-offset-2">
+            <div class="col s10 offset-s2">
                 <div class="jumbotron">
                     <h1>404 Страница не найдена</h1>
                     <p>Страница, на которую вы пытаетесь попасть, вероятно не существует</p>
-                    <p><a class="btn btn-default btn-lg" href="/" role="button">на главную</a></p>
+                    <p><a class="btn btn-default btn-lg" href="{{ url('/') }}" role="button">на главную</a></p>
                 </div>
             </div>
         </div>
