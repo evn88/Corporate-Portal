6355bf626261b94c7291c99f61c26f0c6690236d
Egor <egor@vershkoff.ru>
2017-02-24 12:42:15 +0300

    разделил навигацию для портала и для админки

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,14 +1,14 @@
-@extends('layouts.app')
+@extends((Request::is('admin/*')) ? 'layouts.admin' : 'layouts.app')
 
 @section('content')
-        <div class="container">
-            <div class="col s10 offset-s2">
-                <div class="jumbotron">
-                    <img src="{{ url('img/404.png') }}" class="img img-responsive">
-                    <h1>404 Страница не найдена</h1>
-                    <p>Страница, на которую вы пытаетесь попасть, вероятно не существует</p>
-                    <p><a class="btn btn-default btn-lg" href="{{ url('/') }}" role="button">на главную</a></p>
-                </div>
+    <div class="container">
+        <div class="col s10 offset-s2">
+            <div class="jumbotron">
+                <img src="{{ url('img/404.png') }}" class="img img-responsive">
+                <h1>404 Страница не найдена</h1>
+                <p>Страница, на которую вы пытаетесь попасть, вероятно не существует</p>
+                <p><a class="btn btn-default btn-lg" href="{{ url('/') }}" role="button">на главную</a></p>
             </div>
         </div>
+    </div>
 @endsection
