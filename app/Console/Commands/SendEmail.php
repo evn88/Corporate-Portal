d4c4c209ea9d396672f0a23a597c5c9f497753f0
Vershkov Egor <evn88fx64@gmail.com>
2017-03-10 16:33:51 +0300

    add Queue

---
+++ app/Console/Commands/SendEmail.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Mail;
+use App\User;
+/*use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;*/
+
+
+class SendEmail extends Command //implements ShouldQueue
+{
+     //use InteractsWithQueue, Queueable, SerializesModels;
+     
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:name';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+            $users = \App\News::all();
+            foreach ($users as $user){
+            Mail::raw('Новость: '.$user->title, function($message) {
+                $message->from('noreply@voel.ru', 'Laravel test');
+                $message->to('e.vershkov@voel.ru');
+            });
+            }
+
+    }
+
+}
--- app/Http/Controllers/FileshareController.php
+++ app/Http/Controllers/FileshareController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers;
 use App\Permission;
 use App\Role;
 use Illuminate\Http\Request;
+use Log;
+use Auth;
+use App\Console\Commands\SendEmail;
+use Carbon\Carbon;
+use Queue;
 
 class FileshareController extends Controller
 {
@@ -17,6 +22,15 @@ class FileshareController extends Controller
     {
         //dd(Role::lists('display_name','id'));
         //dd(Permission::find(1));
+        
+        /*$this->dispatch(
+                    Log::info('Проверка работы: '.Auth::user())
+                );
+        */
+        $date = Carbon::now()->addSecond(10);
+
+        Queue::later($date, new SendEmail());
+        //$this->dispatch(new SendEmail())->delay(60);
         return view('fileshare.index');
     }
 }
