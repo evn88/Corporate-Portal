9d519981940c3a2f8a34a822d8d2358d3d624a28
Egor <egor@vershkoff.ru>
2017-03-12 23:22:53 +0300

    text-center

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     <div class="container">
         <div class="col s10 offset-s2">
-            <div class="jumbotron">
+            <div class="jumbotron text-center">
                 <img src="{{ url('img/404.png') }}" class="img img-responsive">
-                <h1>404 Страница не найдена</h1>
+                <h1>Страница не найдена</h1>
                 <p>Страница, на которую вы пытаетесь попасть, вероятно не существует</p>
                 <p><a class="btn btn-default btn-lg" href="{{ url('/') }}" role="button">на главную</a></p>
             </div>
